9b8de4b271a6bdbce9b255beac1502d0a756fd3b
renskiedulog <22104607@usc.edu.ph>
2024-05-25 23:00:23 +0800

    Added: React Type To Bundler

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup';
- 
+
 export default defineConfig({
     format: ['cjs', 'esm'],
-    entry: ['./src/index.ts'],
+    entry: ['./src/index.tsx'], // Entry file with .tsx extension
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
